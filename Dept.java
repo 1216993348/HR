8d5b2effba0b9bf2838109ffc6ee4c760b43cf5e
xhy <1216993348@qq.com>
2017-10-16 22:24:37 +0800

    s

--- Dept.java
+++ Dept.java
@@ -1,23 +1,3 @@
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- public class Hello{
-  	 public static void main(String args[]){
-	 	 System.out.println("dd");
+System.out.println("dd");
 	 }
  }
--- Emp.java
+++ Emp.java
@@ -1,17 +1,3 @@
     public class Hello{
-	public static void main(String args[]){
 
-
-<<<<<<< HEAD
-<<<<<<< HE
-	System.out.println("dev");
->>>>>>>
-=======
-<<<<<<< HEAD  
-	Sys
-=======
-	Syste
->>>>>>> dev
-	}
->>>>>>> dev
 }
